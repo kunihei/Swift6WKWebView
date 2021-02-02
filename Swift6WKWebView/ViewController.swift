7a0361a4fa0ab3dbc08a6d8ea486de376b9e18ee
kunihei <tomarino080@gmail.com>
2021-02-03 08:17:18 +0900

    Initial Commit

---
+++ Swift6WKWebView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6WKWebView
+//
+//  Created by 祥平 on 2021/02/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
